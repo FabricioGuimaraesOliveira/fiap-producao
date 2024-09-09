61e7c76c03ccfff4e86469a75c5dd8dd1d6f9a62
fabricioguimaraesdeoliveira <fabricio.oliveira@partner.ideen.tech>
2024-09-09 17:55:54 -0400

    Ajustando versao spring cloud

--- src/main/java/com/fiap/greentracefood/infrastructure/configuration/AwsConfig.java
+++ src/main/java/com/fiap/greentracefood/infrastructure/configuration/AwsConfig.java
@@ -2,13 +2,9 @@ package com.fiap.greentracefood.infrastructure.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
-
-import java.net.URI;
 
 @Configuration
 public class AwsConfig {
