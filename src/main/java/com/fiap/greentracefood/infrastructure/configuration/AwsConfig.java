039e94bd9b97e4c9618fee0aaf1f3325bd2c21e6
fabricioguimaraesdeoliveira <fabricio.oliveira@partner.ideen.tech>
2024-09-09 16:11:25 -0400

    Configurando sqs client provider

--- src/main/java/com/fiap/greentracefood/infrastructure/configuration/AwsConfig.java
+++ src/main/java/com/fiap/greentracefood/infrastructure/configuration/AwsConfig.java
@@ -3,6 +3,7 @@ package com.fiap.greentracefood.infrastructure.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
@@ -17,6 +18,8 @@ public class AwsConfig {
         return SqsAsyncClient.builder()
                 //.endpointOverride(URI.create("http://localhost:4566"))
                 .region(Region.of("us-east-1"))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+
                 //.credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test")))
                 .build();
     }
